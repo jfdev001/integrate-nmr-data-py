dcb75ffcf59f9990c6a2d9f4afbafee2645ee2c3
Jared Frazier <cscidev001@gmail.com>
2021-02-04 07:29:36 -0600

    Frontend analysis and save button. No fxn yet.

--- frontend/gui.py
+++ frontend/gui.py
@@ -71,35 +71,65 @@ class GUI:
                               text="Click to Choose .ASC File",
                               command=self.fdialog,
                               bg = "bisque")
+                              #width=24)
         self.file_label = tk.Label(self.master, 
                                    textvariable=self.file_name_var,
                                    width=40,
                                    bd=4,
+                                   relief=tk.SUNKEN,
                                    wraplength=280)
+
+        # Analyze button
+        self.analysis_button = tk.Button(self.master,
+                                         text="Click to Analyze Data",
+                                         command=None,
+                                         bg="bisque")
+                                         #width=24)
+
+        # Save button
+        self.save_button = tk.Button(self.master,
+                                     text="Click to Save Analysis",
+                                     command=None,
+                                     bg="bisque")
+                                     #width=24)
+
         #-------End Define Widgets--------
 
         # Grid Widgets to screen
-        self.padding = {"padx": 2, "pady": 2}
+        self.padding = {"padx": 2, "pady": 2, "ipady": 2, "ipadx": 2}
         self.grid_widgets()
 
 
     def grid_widgets(self):
         """Control the geometry of the Widgets."""
         # Title
-        self.title_label.grid(row=0, column=0, sticky=tk.W+tk.E, **self.padding)
-        self.title_entry.grid(row=0, column=1, **self.padding)
+        self.title_label.grid(row=0, column=0, sticky=tk.W+tk.E, 
+                              **self.padding)
+        self.title_entry.grid(row=0, column=1, **self.padding, columnspan=2)
 
         # Limits of integration
-        self.upper_limit_label.grid(row=1, column=0, **self.padding)
-        self.upper_limit_entry.grid(row=1, column=1, **self.padding)
-        self.lower_limit_label.grid(row=2, column=0, **self.padding)
-        self.lower_limit_entry.grid(row=2, column=1, **self.padding)
+        self.upper_limit_label.grid(row=1, column=0, **self.padding, 
+                                    sticky=tk.W+tk.E)
+        self.upper_limit_entry.grid(row=1, column=1, **self.padding, 
+                                    columnspan=2)
+        self.lower_limit_label.grid(row=2, column=0, **self.padding, 
+                                    sticky=tk.W+tk.E)
+        self.lower_limit_entry.grid(row=2, column=1, **self.padding,
+                                    columnspan=2)
 
         # File dialog button
-        self.file_button.grid(row=3, column=0, columnspan=2,
-                              sticky=tk.W+tk.E)
-        self.file_label.grid(row=4, column=0, columnspan=2,
-                             sticky=tk.W+tk.E)
+        self.file_button.grid(row=3, column=0, sticky=tk.W,
+                              ipady=5, ipadx=5, padx=5, pady=5)
+        self.file_label.grid(row=4, column=0, columnspan=3, sticky=tk.W+tk.E,
+                             **self.padding)
+
+        # Data analysis button
+        self.analysis_button.grid(row=3, column=1, sticky=tk.W,
+                                  ipady=5, ipadx=5, padx=5, pady=5)
+
+        # Save button
+        self.save_button.grid(row=3, column=2, sticky=tk.W,
+                              ipady=5, ipadx=5, padx=5, pady=5)
 
     
     def fdialog(self):
@@ -107,8 +137,7 @@ class GUI:
         self.file_path = fd.askopenfilename(title="Select ASC file",
                                                   initialdir=self.home,
                                                   filetypes=self.file_types)
-        fname = "".join(reversed(
-                                 self.file_path[
-                                -1:self.file_path.rfind("/"):-1]))
+        fname = "".join(reversed(self.file_path[
+                                    -1:self.file_path.rfind("/"):-1]))
         self.file_name_var.set(fname)
         
